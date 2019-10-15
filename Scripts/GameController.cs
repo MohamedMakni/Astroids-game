7ed506b06edbcc0c075be7a555941da67ccb7d56
MohamedMakni <mohamed.makni@ensi-uma.tn>
2019-10-15 21:15:47 +0100

    commit

---
+++ Scripts/GameController.cs
@@ -0,0 +1,148 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+//using System;
+
+
+public class GameController : MonoBehaviour
+{
+    public GameObject hazard;
+    public GameObject hazard1;
+    public GameObject enemy;
+    public GameObject enemy1;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+    public Text ScoreText;
+    public Text restartText;
+    public Text gameOverText;
+    public Text highscoreText;
+    Text text;
+
+
+
+    private bool gameOver;
+    private bool restart;
+    private int score;
+    public int highscore;
+
+
+    void Start()
+    {
+        gameOver = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
+        score = 0;
+        UpdateScore();
+        StartCoroutine(SpawnWaves());
+    }
+    void Awake()
+    {
+        text = GetComponent<Text>();
+        score = 0;
+        highscore = PlayerPrefs.GetInt("highscore");
+    }
+
+    void Update()
+    {
+        
+
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                //Application.LoadLevel(Application.loadedLevel);
+                UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+
+
+            }
+        }
+        if (score > highscore)
+        {
+            highscore = score;
+            PlayerPrefs.SetInt("highscore", highscore);
+            highscoreText.text = "Highscore: " + highscore;
+        }
+        text.text = "Score: " + score;
+         
+
+    }
+   
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            
+            for (int i = 0; i < hazardCount; i++)
+            {
+                if (i==9)
+                {
+                    Instantiate(enemy1);
+                }
+                    Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+
+                Vector3 spawnPosition1 = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, -spawnValues.z);
+                Quaternion spawnRotation1 = Quaternion.identity;
+                Instantiate(hazard1, spawnPosition1, spawnRotation1);
+                yield return new WaitForSeconds(spawnWait);
+                if (i == 4)
+                {
+                    Instantiate(enemy);
+                }
+                
+                if (gameOver)
+                {
+
+                    restartText.text = "Restart 'R'";
+                    restart = true;
+                    break;
+                }
+
+
+              
+            }
+                
+            
+               
+            
+            
+            yield return new WaitForSeconds(waveWait);
+
+        }
+    }
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+       /* string[] lines = ;
+        System.IO.File.WriteAllLines(@"score.txt", lines);
+         highscore= Int32.Parse(lines[0].Text)
+            if (highscore < score)
+            {
+                StreamWriter file = new StreamWriter(@"score.txt");
+
+            }*/
+
+        }
+    
+
+    void UpdateScore()
+    {
+        ScoreText.text = "Score: " + score;
+    }
+    public void GameOver()
+    {
+        gameOverText.text = "Game Over!";
+        gameOver = true;
+    }
+        
+   
+}
+ 
---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public float turnSpeed = 50f;
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+    private Rigidbody rb;
+    private AudioSource r;
+    public Boundary boundary;
+
+    private float nextFire;
+
+    void Update()
+    {
+        // if (Input.GetButton("Space") && Time.time> nextFire)
+        if (Input.GetKeyDown("space") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            r = GetComponent<AudioSource>();
+            r.Play();
+        }
+    }
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.DownArrow))
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime/10);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+        rb = GetComponent<Rigidbody>();
+
+        rb.position = new Vector3
+        (
+            Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+        );
+
+    }
+}
